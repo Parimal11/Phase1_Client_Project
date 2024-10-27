ce27e3e61119c6fa49ef38d49cc6021dba3dd148
Parimal11 <anshuldeshmukh6323@gmail.com>
2024-10-27 15:21:45 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { PortfoliosComponent } from './portfolios/portfolios.component';
+import { ProjectTasksComponent } from './project-tasks/project-tasks.component';
+import { ScheduleMeetingComponent } from './schedule-meeting/schedule-meeting.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    PortfoliosComponent,
+    ProjectTasksComponent,
+    ScheduleMeetingComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
